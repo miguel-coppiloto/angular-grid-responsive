64989bd0726b0bece09ec22ee883c6a4d38927cf
Miguel Carrasco Q <miguel@coppiloto.com>
2022-08-05 17:58:58 -0400

    feat(grid): add CDK example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { GridComponent } from './components/grid/grid.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatGridListModule } from '@angular/material/grid-list';
+import { CdkComponent } from './components/cdk/cdk.component';
 
 @NgModule({
-  declarations: [AppComponent, GridComponent],
+  declarations: [AppComponent, GridComponent, CdkComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent],
